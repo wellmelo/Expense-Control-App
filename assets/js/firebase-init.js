9ffd694744ef779cc55d2d02d70db922e5c605da
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-05-17 22:49:32 -0300

    Remove firebase-init gitignore

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyCXSouaJZtOfNiuMYAAeJvtqK8hPXraAk4',
+    authDomain: 'smart-money-86ec3.firebaseapp.com',
+    projectId: 'smart-money-86ec3',
+    storageBucket: 'smart-money-86ec3.appspot.com',
+    messagingSenderId: '366557016162',
+    appId: '1:366557016162:web:c4c059ecf634afb970613b',
+};
+firebase.initializeApp(firebaseConfig);
